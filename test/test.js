d5be02014c00f317f3e76874328c424ff2402f33
Xavi <xavi.rmz@gmail.com>
2011-03-29 21:51:37 -0400

    Made unit tests a bit more robust

--- test/test.js
+++ test/test.js
@@ -1,45 +1,55 @@
 var assert = require("assert");
 var bee = require("../");
 
+var mockResponse = {
+    testCount: 10,
+    end: function() { this.testCount--; }
+}
+
 var line = bee.line({
-    "/test": function(req, res) { assert.equal(req.url, "/test?param=1&woo=2"); },
+    "/test": function(req, res) { assert.equal(req.url, "/test?param=1&woo=2"); res.end(); },
     "/throw-error": function(req, res) { throw Error("503 should catch"); },
     "r`^/name/([\\w-]+)$`": function(req, res, matches) {
         assert.equal(req.url, "/name/woo");
         assert.equal(matches[0], "woo");
+        res.end();
     },
     "`generics`": [ {
             test: function(req) { return req.triggerGeneric; },
-            handler: function(req, res) { assert.ok(req.triggerGeneric); }
+            handler: function(req, res) { assert.ok(req.triggerGeneric); res.end(); }
         }
     ],
     "`404`": function(req, res) {
         assert.equal(req.url, "/url-not-found");
+        res.end();
     },
     "`503`": function(req, res, err) {
         assert.equal(req.url, "/throw-error");
         assert.equal(err.message, "503 should catch");
+        res.end();
     }
 });
-line({ url: "/test?param=1&woo=2" }, {});
-line({ url: "/throw-error" }, {});
-line({ url: "/name/woo" }, {});
-line({ url: "/random", triggerGeneric: true }, {});
-line({ url: "/url-not-found" }, {});
+line({ url: "/test?param=1&woo=2" }, mockResponse);
+line({ url: "/throw-error" }, mockResponse);
+line({ url: "/name/woo" }, mockResponse);
+line({ url: "/random", triggerGeneric: true }, mockResponse);
+line({ url: "/url-not-found" }, mockResponse);
 
 line.add({ 
-    "/ /index": function(req, res) { assert.ok(req.url === "/" || req.url === "/index"); }
+    "/ /index": function(req, res) { assert.ok(req.url === "/" || req.url === "/index"); res.end(); }
 });
-line({ url: "/" }, {});
-line({ url: "/index" }, {});
+line({ url: "/" }, mockResponse);
+line({ url: "/index" }, mockResponse);
 
 line.add({ 
     "/method-test": {
-        "GET": function(req, res) { assert.equal(req.method, "GET"); },
-        "POST": function(req, res) { assert.equal(req.method, "POST"); },
-        "any": function(req, res) { assert.ok(req.method !== "GET" || req.method !== "POST"); }
+        "GET": function(req, res) { assert.equal(req.method, "GET"); res.end(); },
+        "POST": function(req, res) { assert.equal(req.method, "POST"); res.end(); },
+        "any": function(req, res) { assert.ok(req.method !== "GET" || req.method !== "POST"); res.end(); }
     }
 });
-line({ url: "/method-test", method: "GET" }, {});
-line({ url: "/method-test", method: "POST" }, {});
-line({ url: "/method-test", method: "HEAD" }, {});
+line({ url: "/method-test", method: "GET" }, mockResponse);
+line({ url: "/method-test", method: "POST" }, mockResponse);
+line({ url: "/method-test", method: "HEAD" }, mockResponse);
+
+assert.ok(mockResponse.testCount === 0);
